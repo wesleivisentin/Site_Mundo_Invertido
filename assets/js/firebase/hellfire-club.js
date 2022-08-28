a6916c02f3977f3f8184b0131554ceb22fcb9e62
weslei <wesleivisentin@hotmail.com>
2022-08-28 19:31:06 -0300

    Update hellfire-club.js

--- assets/js/firebase/hellfire-club.js
+++ assets/js/firebase/hellfire-club.js
@@ -8,3 +8,19 @@ export async function subscribeToHellfireClun(subscription) {
     const docRef = await addDoc(hellfireClubCollection, subscription)
     return docRef.id
 }
+
+export async function getHellfireClubSubscriptions() {
+    const db = getFirestore(app)
+    const hellfireClubCollection = collection(db, "hellfire-clube")
+    const subscriptions = hellfireClubCollection.docs.map(doc => doc.data());
+    return subscriptions;
+  }
+
+
+// CODIGO TIRADO DA DOCUMENTAÇÃO PARA EDITAR O DE CIMA:
+// async function getCities(db) {
+//     const citiesCol = collection(db, 'cities');
+//     const citySnapshot = await getDocs(citiesCol);
+//     const cityList = citySnapshot.docs.map(doc => doc.data());
+//     return cityList;
+//   }
